5c15a462c50a98be633c66267199bca6a05fa077
kapil namdeo <kapilnamdeo2003@gmail.com>
2025-02-24 13:04:17 +0530

    [Kapil Namdeo]Add.Modify EmployeeService class to store data in arraylist instead of storing in database H2

--- src/main/java/com/bridgelabz/EmployeePayrollApp/service/EmployeeService.java
+++ src/main/java/com/bridgelabz/EmployeePayrollApp/service/EmployeeService.java
@@ -6,12 +6,16 @@ import com.bridgelabz.EmployeePayrollApp.repository.EmployeeRepository;
 import com.bridgelabz.EmployeePayrollApp.validation.EmployeeNotFoundException;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
-import java.util.List;
 
+import java.util.ArrayList;
+import java.util.List;
+import java.util.concurrent.atomic.AtomicLong;
 
 
 @Service
 public class EmployeeService {
+    //uc2
+    /*
     @Autowired
     private EmployeeRepository repository;
 
@@ -41,4 +45,53 @@ public class EmployeeService {
         }
         repository.deleteById(id);
     }
+
+    */
+
+
+
+
+    /* UC-03 Ability for the Services Layer to store the Employee Payroll Data - Note that Services Layer is not storing, updating
+or deleting Employee Payroll Data in UC 2.- In this use case the Services Layer will store this Data in a Memory as a List. As we go further
+down we will persist this data in DB.*/
+
+    private final List<Employee> employeeList = new ArrayList<>();
+    private final AtomicLong idCounter = new AtomicLong(1); // Generates unique IDs
+
+    public List<Employee> getAllEmployees() {
+        return employeeList;
+    }
+
+    public Employee getEmployeeById(Long id) {
+        return employeeList.stream()
+                .filter(emp -> emp.getId().equals(id))
+                .findFirst()
+                .orElseThrow(() -> new EmployeeNotFoundException("Employee not found with ID: " + id));
+    }
+
+    public Employee createEmployee(EmployeeDTO employeeDTO) {
+        Employee employee = new Employee();
+        employee.setId(idCounter.getAndIncrement()); // Assign unique ID
+        employee.setName(employeeDTO.getName());
+        employee.setSalary(employeeDTO.getSalary());
+        employeeList.add(employee);
+        return employee;
+    }
+
+    public Employee updateEmployee(Long id, EmployeeDTO employeeDTO) {
+        Employee existingEmployee = getEmployeeById(id); // Find existing employee
+        existingEmployee.setName(employeeDTO.getName());
+        existingEmployee.setSalary(employeeDTO.getSalary());
+        return existingEmployee;
+    }
+
+    public void deleteEmployee(Long id) {
+        Employee existingEmployee = getEmployeeById(id);
+        employeeList.remove(existingEmployee);
+    }
+
+    // As we go further
+    //down we will persist this data in DB.
+
+
 }
