b14218079030aca9c0f27a74c9e3299b1737a274
kapil namdeo <kapilnamdeo2003@gmail.com>
2025-02-25 11:08:17 +0530

    [Kapil Namdeo]Add. Lombok’s @Slf4j annotation for logger

--- src/main/java/com/bridgelabz/EmployeePayrollApp/EmployeePayrollAppApplication.java
+++ src/main/java/com/bridgelabz/EmployeePayrollApp/EmployeePayrollAppApplication.java
@@ -1,12 +1,15 @@
 package com.bridgelabz.EmployeePayrollApp;
 
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+@Slf4j
 @SpringBootApplication
 public class EmployeePayrollAppApplication {
 	public static void main(String[] args) {
 		SpringApplication.run(EmployeePayrollAppApplication.class, args);
+		log.info("Employee Payroll Application has started successfully.");
 	}
 }
 
