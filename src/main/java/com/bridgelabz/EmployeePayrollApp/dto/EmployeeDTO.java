5874b46a484a0231536dfb3651b22c1317cb394b
kapil namdeo <kapilnamdeo2003@gmail.com>
2025-02-25 13:57:53 +0530

    [Kapil Namdeo]Add.Add Validation to Name Field the REST call can be validated

--- src/main/java/com/bridgelabz/EmployeePayrollApp/dto/EmployeeDTO.java
+++ src/main/java/com/bridgelabz/EmployeePayrollApp/dto/EmployeeDTO.java
@@ -4,14 +4,19 @@ package com.bridgelabz.EmployeePayrollApp.dto;
 
 
 
+import jakarta.validation.constraints.NotEmpty;
+import jakarta.validation.constraints.Pattern;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
+//Section4 uc 1 validation added
 
 @Data  // Generates getters, setters, equals, hashCode, and toString
 @NoArgsConstructor // Generates a no-args constructor
 @AllArgsConstructor // Generates an all-args constructor
 public class EmployeeDTO {
+    @NotEmpty(message = "Name cannot be empty")
+    @Pattern(regexp = "^[A-Z][a-zA-Z ]{2,}$", message = "Name must start with a capital letter and have at least 3 characters")
     private String name;
     private double salary;
 }
